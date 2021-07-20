ed5230e0855e44f1d1ac0e6298ef7a6f4f0c81b8
douglasmsgmatos <87094368+douglasmsgmatos@users.noreply.github.com>
2021-07-20 19:40:30 -0300

    Add files via upload

---
+++ greeting.rb
@@ -0,0 +1,24 @@
+class Greeting 
+    def hi(pessoa)
+        puts "Olá, #{pessoa}! Que seu dia seja lindo!"
+    end
+
+    def to_s
+        "Classe Greeting"
+    end
+end
+
+gr = Greeting.new
+gr.hi("Marcelo")
+gr.hi("Paulo Lins")
+gr.hi("Jackson Lima")
+gr.hi("Ana Paula")
+gr.hi("Joao Gilberto")
+gr.hi("Franco Caldas")
+gr.hi("Bruno Bastos")
+gr.hi("Jessica")
+gr.hi("Abraão Carvalho")
+gr.hi("Lucas Lucatero")
+gr.hi("Breno Castellani")
+gr.hi("Lucas Galli")
+gr.hi("Douglas Martins")
