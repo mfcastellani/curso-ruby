2cac52de0578d2ebb449225cceeaf6c2601f8332
jonjo <joaogilberto@tuta.io>
2021-07-22 13:54:48 -0300

    Versão 3 usando while true do

--- aula-02/guessing-game/guessing_game.rb
+++ aula-02/guessing-game/guessing_game.rb
@@ -5,27 +5,27 @@
 def escolher
     puts 'Tente adivinhar o número de 0 a 10 ou digite 99 para sair'
     @escolha = gets.chomp.to_i
-    if @escolha > 10
+    if @escolha == 99
+        puts 'Você saiu do programa.'
+        exit 0
+    elsif @escolha > 10
         puts 'Número maior que o permitido'
         escolher
     elsif @escolha.negative?
         puts 'Número menor que o permitido'
         escolher
-    elsif @escolha == 99
-        puts 'Você saiu do programa.'
+    
     end
 end
 
-escolher
-
-if @escolha == @number
-    puts 'ACERTOU!'
-elsif @escolha > @number
-    puts 'O número é menor do que esse.'
+while true do
     escolher
-elsif @escolha < @number
-    puts 'O número é maior do que esse.'
-    escolher
+    if @escolha == @number
+        puts 'ACERTOU!'
+        break
+    elsif @escolha > @number
+        puts 'O número é menor do que esse.'
+    elsif @escolha < @number
+        puts 'O número é maior do que esse.'
+    end
 end
-
-
