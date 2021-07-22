8a00080dbe904e232f14ae29abef6b6b90100884
jonjo <joaogilberto@tuta.io>
2021-07-21 22:32:46 -0300

    Adcionada a opção 99 - sair

--- aula-02/guessing-game/guessing_game.rb
+++ aula-02/guessing-game/guessing_game.rb
@@ -3,13 +3,15 @@
 number = rand(10)
 
 def escolher
-    puts 'Tente adivinhar o número de 0 a 10'
+    puts 'Tente adivinhar o número de 0 a 10 ou digite 99 para sair'
     @escolha = gets.chomp.to_i
 end
 
 escolher
 
-if @escolha > number
+if @escolha == 99
+    puts "Você saiu do programa."
+elsif @escolha > number
     puts 'O número é menor do que esse.'
     escolher
 elsif @escolha < number
