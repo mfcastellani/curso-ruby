5aa0e6504044158bfc34e14382e06cffa4f6f73a
jonjo <joaogilberto@tuta.io>
2021-07-21 22:52:32 -0300

    Organização do código em duas partes

--- aula-02/guessing-game/guessing_game.rb
+++ aula-02/guessing-game/guessing_game.rb
@@ -1,22 +1,31 @@
 # frozen_string_literal: true
 
-number = rand(10)
+@number = rand(10)
 
 def escolher
     puts 'Tente adivinhar o número de 0 a 10 ou digite 99 para sair'
     @escolha = gets.chomp.to_i
+    if @escolha > 10
+        puts 'Número maior que o permitido'
+        escolher
+    elsif @escolha.negative?
+        puts 'Número menor que o permitido'
+        escolher
+    elsif @escolha == 99
+        puts 'Você saiu do programa.'
+    end
 end
 
 escolher
 
-if @escolha == 99
-    puts "Você saiu do programa."
-elsif @escolha > number
+if @escolha == @number
+    puts 'ACERTOU!'
+elsif @escolha > @number
     puts 'O número é menor do que esse.'
     escolher
-elsif @escolha < number
+elsif @escolha < @number
     puts 'O número é maior do que esse.'
     escolher
-else
-    puts 'ACERTOU!'
 end
+
+
