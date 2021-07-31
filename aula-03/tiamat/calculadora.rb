b82551fcfb71580baca2dea92722a7f0eae1791f
Franco Caldas <francocaldas@gmail.com>
2021-07-30 21:53:32 -0300

    implementa o método limpar - reseta a calculadora quando o usuário teclar a letra c

--- aula-03/tiamat/calculadora.rb
+++ aula-03/tiamat/calculadora.rb
@@ -22,7 +22,9 @@ def dividir(num1, num2)
 end
 
 def limpar
-  loop?
+  puts "Você resetou a calculadora"
+  @val1 = 0
+  true
 end
 
 def sair
@@ -32,33 +34,39 @@ end
 # Fazer o prompt do programa. Texto de entrada
 puts 'A qualquer momento use x para sair ou c para resetar a calculadora'
 
-val1 = 0
+@val1 = 0
 
 loop do
   # fazer um if para ver se quer limpar usando a letra "C" a qualquer momento
-  if val1.zero?
+  if @val1.zero?
     puts 'Tecle o primeiro valor:'
-    val1 = gets.chomp
-    sair if val1 == 'x'
+    @val1 = gets.chomp
+    sair if @val1 == 'x'
+    # Reseta a calculadora caso o usuário tecle 'C'
+    next if @val1.downcase == 'c' && limpar
   end
 
   puts 'Escolha um dos operadores: + - * ou /:'
-  operador = gets.chomp
-  sair if operador == 'x'
+  @operador = gets.chomp
+  sair if @operador == 'x'
+  # Reseta a calculadora caso o usuário tecle 'C'
+  next if @operador.downcase == 'c' && limpar
   # Fazer a chamada para saída ou limpar
 
   puts 'Tecle o valor:'
-  val2 = gets.chomp
-  sair if val2 == 'x'
+  @val2 = gets.chomp
+  sair if @val2 == 'x'
+  # Reseta a calculadora caso o usuário tecle 'C'
+  next if @val2.downcase == 'c' && limpar
   # Fazer a chamada para saída ou limpar
 
-  resultado = somar(val1.to_f, val2.to_f) if operador == '+'
-  resultado = diminuir(val1.to_f, val2.to_f) if operador == '-'
-  resultado = multiplicar(val1.to_f, val2.to_f) if operador == '*'
-  resultado = dividir(val1.to_f, val2.to_f) if operador == '/'
+  @resultado = somar(@val1.to_f, @val2.to_f) if @operador == '+'
+  @resultado = diminuir(@val1.to_f, @val2.to_f) if @operador == '-'
+  @resultado = multiplicar(@val1.to_f, @val2.to_f) if @operador == '*'
+  @resultado = dividir(@val1.to_f, @val2.to_f) if @operador == '/'
 
-  puts "O resultado é #{resultado}"
+  puts "O resultado é #{@resultado}"
 
-  val1 = resultado
+  @val1 = @resultado
   # método para transformar vírgula em ponto
 end
