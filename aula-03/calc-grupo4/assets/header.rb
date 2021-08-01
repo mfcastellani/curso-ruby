b9d4c9ab69d1ea877b5fcb95addb6ba7dd0ad3c5
pauloalchemist <paulolinsdev@gmail.com>
2021-08-01 20:27:34 -0300

    separating presentation header

---
+++ aula-03/calc-grupo4/assets/header.rb
@@ -0,0 +1,23 @@
+def head
+  calc_head = <<HEAD
+   ========================================#{' '}
+   |       Calculadora MegaBlaster        |#{' '}
+   |    ------------------------------    |#{' '}
+   |       Tem um hobbit saindo da        |#{' '}
+   |         toca. Você consegue          |#{' '}
+   |       adivinhar qual hobbit é?       |#{' '}
+   |                                      |#{' '}
+   |       Será Bilbo, Frodo, Sam,        |#{' '}
+   |          Merry ou Pippin?            |#{' '}
+   |                                      |#{' '}
+   |       Que a senhora de Lothlórien    |#{' '}
+   |       lhe ajude, pois os danados     |#{' '}
+   |               são rápidos!           |#{' '}
+   |                                      |#{' '}
+   |                              Grupo4  |#{' '}
+   ========================================#{' '}
+   A calculadora está ativa...#{' '}
+   Pressione Ctrl+C para sair a qualquer momento#{' '}
+
+HEAD
+end
