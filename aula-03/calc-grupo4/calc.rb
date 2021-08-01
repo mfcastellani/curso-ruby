bd81c0b4c6a2f473cfeb5808acd508941019bdc6
pauloalchemist <paulolinsdev@gmail.com>
2021-08-01 16:50:42 -0300

    main script

---
+++ aula-03/calc-grupo4/calc.rb
@@ -0,0 +1,15 @@
+require_relative './lib/lib_calc'
+require 'awesome_print'
+
+calc = Calc.new
+
+puts 'Digite o primeiro numero: '
+num1 = gets.chomp.to_f
+puts 'Digite o segundo número: '
+num2 = gets.chomp.to_f
+
+puts num1
+
+result = calc.add(num1, num2)
+puts "A soma é: #{result}"
+ap result
