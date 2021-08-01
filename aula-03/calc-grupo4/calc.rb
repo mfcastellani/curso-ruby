2b452e60eacf1930056eba03de4255526b258cee
pauloalchemist <paulolinsdev@gmail.com>
2021-08-01 18:25:28 -0300

    renaming method from add to sum

--- aula-03/calc-grupo4/calc.rb
+++ aula-03/calc-grupo4/calc.rb
@@ -10,6 +10,6 @@ num1 = gets.chomp.to_f
 puts 'Digite o segundo número: '
 num2 = gets.chomp.to_f
 
-result = calc.add(num1, num2)
+result = calc.sum(num1, num2)
 puts "A soma é: #{result}"
 ap result
