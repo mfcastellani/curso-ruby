f3cd96af61720b8c85594b550459d538f682ad3d
pauloalchemist <paulolinsdev@gmail.com>
2021-08-02 19:52:32 -0300

    formatting corrected with rubocop

--- aula-03/calc-grupo4/lib/lib_calc.rb
+++ aula-03/calc-grupo4/lib/lib_calc.rb
@@ -3,20 +3,19 @@ class Calc
     # fazer soma de dois números
     num1 + num2
   end
-  
-  def substract(num1,num2)
+
+  def substract(num1, num2)
     # fazer soma de dois números
     num1 - num2
   end
 
-  def multiply(num1,num2)
+  def multiply(num1, num2)
     # fazer multiplicação
     num1 * num2
   end
 
-  def divide(num1,num2)
-    #fazer divisao
+  def divide(num1, num2)
+    # fazer divisao
     num1 / num2
-end
-
+  end
 end
