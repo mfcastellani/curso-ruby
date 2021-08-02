02b49a18bd33598456b9b2a50765b29e53eb8d44
mirandadiscos <mirandaguedes.victor@gmail.com>
2021-08-02 09:02:21 -0300

    Adicionando novas features(MULT,DIVISÃO,SUBTRAÇÃO)

--- aula-03/calc-grupo4/lib/lib_calc.rb
+++ aula-03/calc-grupo4/lib/lib_calc.rb
@@ -3,4 +3,20 @@ class Calc
     # fazer soma de dois números
     num1 + num2
   end
+  
+  def substract(num1,num2)
+    # fazer soma de dois números
+    num1 - num2
+  end
+
+  def multiply(num1,num2)
+    # fazer multiplicação
+    num1 * num2
+  end
+
+  def divide(num1,num2)
+    #fazer divisao
+    num1 / num2
+end
+
 end
