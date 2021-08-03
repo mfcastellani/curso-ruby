147c110e9210f122d0011e6ae255b371fcb482a3
Micael Marques <micasmarques1132@gmail.com>
2021-08-02 21:53:17 -0300

    Alterações feitas por Micael Marques, onde deixei a Calculadora quase pronta, estando apenas com um problema de loop após a conta

--- aula-03/calc-grupo4/calc.rb
+++ aula-03/calc-grupo4/calc.rb
@@ -2,16 +2,51 @@
 
 require_relative './assets/header'
 require_relative './lib/lib_calc'
-require 'awesome_print'
 
-puts head.yellow
+puts "Calculadora"
+puts "-----------"
+
+puts "Escolha a operacao!"
+puts "1 Adicao"
+puts "2 Subtracao"
+puts "3 Divisao"
+puts "4 Multiplicacao"
+puts "0 Para sair"
 
 calc = Calc.new
 
-puts 'Digite o primeiro número: '.blue
-num1 = gets.chomp.to_f
-puts 'Digite o segundo número: '.blue
-num2 = gets.chomp.to_f
+puts 'Digite o numero da operação que será feita: '
+operacao = gets.chomp.to_f
+puts 'Digite o primeiro número para participar da operação: '
+n1 = gets.chomp.to_f
+puts 'Digite o segundo número para participar da operação: '
+n2 = gets.chomp.to_f
 
-result = calc.sum(num1, num2)
-puts "O resultado da soma é #{result}".green
+
+
+while true
+    if operacao == 1
+        puts "O resultado da soma é #{calc.sum(n1, n2)}"
+        redo
+    end    
+    if operacao == 2
+        puts "O resultado da subtração é #{calc.subtract(n1, n2)}"
+        redo
+    end
+    if operacao == 3
+        puts "O resultado da divisão é #{calc.divide(n1, n2)}"
+        redo
+    end
+    if operacao == 4
+        puts "O resultado da multiplicação é #{calc.multiply(n1, n2)}"
+        redo
+    end
+    if operacao == ""
+        calculo.null
+        redo
+    end
+    if operacao == 0
+        break
+    end
+end
+
