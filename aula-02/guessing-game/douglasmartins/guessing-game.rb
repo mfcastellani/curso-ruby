de830d332286deca6bbd1257534c496516345ba4
Douglas Martins <douglas.matosje@gmail.com>
2021-08-04 15:27:50 -0300

    Alteraçao da estrutura de repetiçao e textos de entrada

--- aula-02/guessing-game/douglasmartins/guessing-game.rb
+++ aula-02/guessing-game/douglasmartins/guessing-game.rb
@@ -1,28 +1,28 @@
 def menu
-    puts "1 - INICIAR O JOGO"
-    puts "2 - SAIR"
+  puts "##################### JOGO DA ADIVINHAÇÃO #############################"
+  puts "Escolha a OPÇÃO:"
+  puts "1 - INICIAR O JOGO"
+  puts "0 - SAIR"
 end
 
-puts "##################### JOGO DA ADIVINHAÇÃO #############################"
-puts "MENU"
+loop do
+  menu
+  menu_opcao = gets.chomp.to_i
 
-menu
-
-puts "Escolha a OPÇÃO:"
-menu_opcao = gets.chomp.to_i
-
-case menu_opcao 
+  case menu_opcao
+  when 0
+    puts "PROGRAMA ENCERRADO"
+    exit 0
   when 1
     range_number = rand(0..10)
-    puts "Tente adivinhar quantas medalhas de ouro o Brasil terá nas OLIMPÍADAS: "
+    puts "Tente adivinhar quantas medalhas de ouro o Brasil terá nas OLIMPÍADAS (entre 1 e 9): "
     adv_number = gets.chomp.to_i
     while range_number != adv_number do
       puts "NÃO ACERTOU! CONTINUE TENTANDO ..."
       adv_number = gets.chomp.to_i
     end
-    puts "PARABÉNS!!! VOCÊ ACERTOU!!!"
-  when 2
-    exit 0
-  else
-     puts "OPÇÃO INEXISTENTE!"
+      puts "PARABÉNS!!! VOCÊ ACERTOU!!!"
+    else
+      puts "OPÇÃO INEXISTENTE!"
   end
+end
