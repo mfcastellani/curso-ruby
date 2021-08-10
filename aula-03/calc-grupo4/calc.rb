90df50333434b79feacb99da92efa7fde48348ca
pauloalchemist <paulolinsdev@gmail.com>
2021-08-10 10:29:15 -0300

    add loop do and error catch

--- aula-03/calc-grupo4/calc.rb
+++ aula-03/calc-grupo4/calc.rb
@@ -1,52 +1,45 @@
 #! /usr/bin/env ruby
 
+require_relative './assets/menu'
 require_relative './assets/header'
 require_relative './lib/lib_calc'
+require 'awesome_print'
 
-puts "Calculadora"
-puts "-----------"
-
-puts "Escolha a operacao!"
-puts "1 Adicao"
-puts "2 Subtracao"
-puts "3 Divisao"
-puts "4 Multiplicacao"
-puts "0 Para sair"
+puts head.yellow
 
 calc = Calc.new
 
-puts 'Digite o numero da operação que será feita: '
-operacao = gets.chomp.to_f
-puts 'Digite o primeiro número para participar da operação: '
-n1 = gets.chomp.to_f
-puts 'Digite o segundo número para participar da operação: '
-n2 = gets.chomp.to_f
+loop do
+  puts available_operations.white
 
+  operation = gets.chomp
 
+  if operation.casecmp?('E')
+    puts 'Bye bye!!'.white
+    exit(0)
+  end
 
-while true
-    if operacao == 1
-        puts "O resultado da soma é #{calc.sum(n1, n2)}"
-        redo
-    end    
-    if operacao == 2
-        puts "O resultado da subtração é #{calc.subtract(n1, n2)}"
-        redo
-    end
-    if operacao == 3
-        puts "O resultado da divisão é #{calc.divide(n1, n2)}"
-        redo
-    end
-    if operacao == 4
-        puts "O resultado da multiplicação é #{calc.multiply(n1, n2)}"
-        redo
-    end
-    if operacao == ""
-        calculo.null
-        redo
-    end
-    if operacao == 0
-        break
-    end
+  puts 'Digite o primeiro número: '.blue
+  num1 = gets.chomp.to_f
+  puts 'Digite o segundo número: '.blue
+  num2 = gets.chomp.to_f
+
+  # sleep 5
+
+  result = case operation
+           when '+', 'Somar'
+             a = calc.sum(num1, num2)
+             puts "O resultado da soma é: #{a}".green
+           when '/', 'Dividir'
+             b = calc.divide(num1, num2)
+             puts "O resultado da divisão é: #{b}".green
+           when '*', 'Multiplicar'
+             c = calc.multiply(num1, num2)
+             puts "O resultado da multiplicação é: #{c}".green
+           when '-', 'Subtrair'
+             d = calc.substract(num1, num2)
+             puts "O resultado da subtração é: #{d}".green
+           end
+rescue StandardError => e
+  puts "Erro ao calcular: #{e.message}".red
 end
-
