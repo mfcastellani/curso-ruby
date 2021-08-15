f5d5c7521b517a6b38e28fffa7ec08f5f163513d
pauloalchemist <paulolinsdev@gmail.com>
2021-08-15 16:50:49 -0300

    defined class and its properties

--- aula-05/dragonfusion/address_books/lib/contact.rb
+++ aula-05/dragonfusion/address_books/lib/contact.rb
@@ -1,3 +1,9 @@
-def new_person
-  puts 'crie uma pessoa!'
+class Contact
+  attr_accessor :name, :contact, :cpf
+
+  def initialize(name, contact, cpf)
+    @name = name
+    @contact = contact
+    @cpf = cpf
+  end
 end
