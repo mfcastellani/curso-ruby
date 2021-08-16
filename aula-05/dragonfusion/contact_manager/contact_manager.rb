d15ff33214f303cd7adca78c135071bcd2e195e3
pauloalchemist <paulolinsdev@gmail.com>
2021-08-16 18:17:02 -0300

    better formatted error message

--- aula-05/dragonfusion/contact_manager/contact_manager.rb
+++ aula-05/dragonfusion/contact_manager/contact_manager.rb
@@ -2,13 +2,14 @@ require 'cli/ui'
 require_relative './lib/contact'
 
 loop do
+  puts ''
   CLI::UI::Prompt.ask('O que você deseja fazer?') do |handler|
-    handler.option('Consultar todos os registros') { |_selection| listing_all_contacts }
-    handler.option('Incluir uma pessoa na agenda') { |_selection| new_contact }
+    handler.option('Consultar todos os registros') { listing_all_contacts }
+    handler.option('Incluir uma pessoa na agenda') { new_contact }
     handler.option('Alterar dados de uma pessoa') { |selection| selection }
     handler.option('Excluir os dados de uma pessoa') { |selection| selection }
-    handler.option('Sair') { |_selection| exit_cm }
+    handler.option('Sair') { exit_cm }
   end
 rescue StandardError => e
-  puts "Ocorreu um erro: #{e.message}"
+  puts CLI::UI.fmt "⚠️ {{underline:Ocorreu um erro:}}#{e.message}"
 end
--- aula-05/dragonfusion/contact_manager/lib/contact.rb
+++ aula-05/dragonfusion/contact_manager/lib/contact.rb
@@ -4,25 +4,25 @@ require 'cli/ui'
 @address_book = []
 
 def new_contact
-  puts 'Insira o nome do contato: '
+  puts CLI::UI.fmt '{{blue:Insira o nome do contato:}}'
   name = gets.chomp
-  puts 'Insira seu telefone ou e-mail: '
+  puts CLI::UI.fmt '{{blue:Insira seu telefone ou e-mail:}}'
   contact = gets.chomp
-  puts 'Insira o seu CPF: '
+  puts CLI::UI.fmt '{{blue:Insira o seu CPF:}}'
   cpf = gets.chomp
 
   if @address_book.any? { |contact| contact[:cpf] == cpf }
-    raise 'Este CPF já está cadastrado na sua agenda'
+    raise CLI::UI.fmt '{{red: Este CPF já está cadastrado na sua agenda}}'
   else
     insert_contact = { name: name, contact: contact, cpf: cpf }
     @address_book << insert_contact
-    puts CLI::UI.fmt "Inclusão de {{green:#{name}}} com o contato {{green:#{contact}}} e CPF {{green:#{cpf}}} feita com sucesso!"
+    puts CLI::UI.fmt "{{v}} Inclusão de {{green:#{name}}} com o contato {{green:#{contact}}} e CPF {{green:#{cpf}}} feita com sucesso!"
   end
 end
 
 def listing_all_contacts
+  puts CLI::UI.fmt '{{yellow:Listando todos os contato:}}'
   @address_book.each do |contact|
-    puts 'Listando todos os contato: '
     puts "Nome: #{contact[:name]} | Contato: #{contact[:contact]} | CPF: #{contact[:cpf]}"
   end
 end
