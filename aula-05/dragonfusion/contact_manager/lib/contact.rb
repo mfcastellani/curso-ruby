bd4b9a697efad0355561053516f0a8a5d2596a20
pauloalchemist <paulolinsdev@gmail.com>
2021-08-17 12:30:45 -0300

    cpf validation working

--- aula-05/dragonfusion/contact_manager/lib/contact.rb
+++ aula-05/dragonfusion/contact_manager/lib/contact.rb
@@ -11,6 +11,9 @@ def new_contact
   puts CLI::UI.fmt '{{blue:Insira o seu CPF:}}'
   cpf_prompt = gets.chomp
   cpf = CPF.new(cpf_prompt)
+
+  raise CLI::UI.fmt '{{red: CPF inválido. Verifique o dado e tente novamente}}' if cpf.valid? != true
+
   cpf_formatted = cpf.formatted
 
   if @address_book.any? { |contact| contact[:cpf] == cpf_formatted }
@@ -27,24 +30,6 @@ def listing_all_contacts
   puts ''
   @address_book.each do |contact|
     puts CLI::UI.fmt "{{bold:Nome: }}{{cyan:#{contact[:name]} }}{{bold:|}} {{bold:Contato: }}{{cyan:#{contact[:contact]} }}{{bold:|}} {{bold:CPF: }}{{cyan:#{contact[:cpf]}}}"
-  end
-end
-
-def list_contact_by_key; end
-
-def change_contact_by_cpf
-  puts CLI::UI.fmt '{{bold:Digite o CPF do contato que deseja alterar:}}'
-  cpf = gets.chomp
-  puts CLI::UI.fmt '{{bold:Qual campo deseja alterar: Nome ou email?}}'
-  option = gets.chomp.downcase
-  puts CLI::UI.fmt '{{bold:Alterando contato por CPF:}}'
-  puts ''
-
-  if @address_book.any? { |contact| contact[:cpf] == cpf }
-
-    puts CLI::UI.fmt "{{v}} Nome alterado para {{green:#{name}}} com o contato {{green:#{contact}}} e CPF {{green:#{cpf}}} feita com sucesso!"
-  else
-    raise CLI::UI.fmt '{{red: Este CPF não está cadastrado na sua agenda}}'
   end
 end
 
