a4c01f3fce6597530df69badff47dd6a5ab404cd
Lucas Galli <lucas.galli@gmail.com>
2021-08-16 21:45:05 -0300

    Interface changes and CPF validation
    CPF validation is working but the number itself is not being stored correctly. I couldn't
    solve this problem by myself.

--- aula-05/dragonfusion/contact_manager/contact_manager.rb
+++ aula-05/dragonfusion/contact_manager/contact_manager.rb
@@ -1,12 +1,16 @@
 require 'cli/ui'
 require_relative './lib/contact'
 
-loop do
+puts "\e[H\e[2J"
+
+loop do 
+  puts '*** RUBY CONTACT MANAGER ***'
   CLI::UI::Prompt.ask('O que você deseja fazer?') do |handler|
-    handler.option('Consultar todos os registros') { |_selection| listing_all_contacts }
-    handler.option('Incluir uma pessoa na agenda') { |_selection| new_contact }
-    handler.option('Alterar dados de uma pessoa') { |selection| selection }
-    handler.option('Excluir os dados de uma pessoa') { |selection| selection }
+    handler.option('Buscar um contato') { |selection| selection }
+    handler.option('Incluir um contato') { |_selection| new_contact }
+    handler.option('Modificar um contato') { |selection| selection }
+    handler.option('Excluir um contato') { |selection| selection }
+    handler.option('Exibir todos os contatos') { |_selection| listing_all_contacts }
     handler.option('Sair') { |_selection| exit_cm }
   end
 rescue StandardError => e
--- aula-05/dragonfusion/contact_manager/lib/contact.rb
+++ aula-05/dragonfusion/contact_manager/lib/contact.rb
@@ -3,26 +3,46 @@ require 'cpf_cnpj'
 @address_book = []
 
 def new_contact
-  puts 'Insira o nome do contato: '
+  puts '*** Criando novo contato ***'
+  puts ''
+  puts 'Insira o nome do contato'
+  print '> '
   name = gets.chomp
-  puts 'Insira seu telefone ou e-mail: '
+  puts ''
+  puts 'Insira o telefone ou e-mail do contato'
+  print '> '
   contact = gets.chomp
-  puts 'Insira o seu CPF: '
+  puts ''
+  puts 'Insira o CPF do contato (apenas números)'
+  print '> '
   cpf = gets.chomp
-
-  if @address_book.any? { |contact| contact[:cpf] == cpf }
-    raise 'Este CPF já está cadastrado na sua agenda'
-  else
-    insert_contact = { name: name, contact: contact, cpf: cpf }
-    @address_book << insert_contact
-    puts "Inclusão de #{name} com o contato #{contact} e CPF #{cpf} feita com sucesso!"
+  puts ''
+  cpf = CPF.new(cpf)
+  
+  until cpf.valid? == true
+    puts "CPF inválido! Digite um número válido de CPF."
+    print '> '
+    cpf = gets.chomp
+    cpf = CPF.new(cpf)
+    puts ''
   end
+    if @address_book.any? { |contact| contact[:cpf] == cpf }
+      raise 'Este CPF já está cadastrado na sua agenda!'
+    else
+      insert_contact = { name: name, contact: contact, cpf: cpf }
+      @address_book << insert_contact
+      puts CLI::UI.fmt "Inclusão de {{green:#{name}}} com o contato {{green:#{contact}}} e CPF {{green:#{cpf}}} feita com sucesso!"
+    end
 end
 
 def listing_all_contacts
+  puts "\e[H\e[2J"
+  puts '*** Exibindo todos os contatos ***'
+  puts ''
   @address_book.each do |contact|
-    puts 'Listando todos os contato: '
     puts "Nome: #{contact[:name]} | Contato: #{contact[:contact]} | CPF: #{contact[:cpf]}"
+    puts ''
+# Insert a prompt for a key before returning to the main menu
   end
 end
 
