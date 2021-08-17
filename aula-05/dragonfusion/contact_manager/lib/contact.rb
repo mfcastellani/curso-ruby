f8ec6cef226f2a0a14056b15d62c26a6aa11924b
pauloalchemist <paulolinsdev@gmail.com>
2021-08-17 19:31:15 -0300

    changing the name and means of contact through cpf is working

--- aula-05/dragonfusion/contact_manager/lib/contact.rb
+++ aula-05/dragonfusion/contact_manager/lib/contact.rb
@@ -1,6 +1,6 @@
 require 'cpf_cnpj'
 require 'cli/ui'
-require_relative './cpf_treatment'
+require_relative './cpf_handler'
 
 @address_book = []
 
@@ -27,6 +27,31 @@ def listing_all_contacts
   @address_book.each do |contact|
     puts CLI::UI.fmt "{{bold:Nome: }}{{cyan:#{contact[:name]} }}{{bold:|}} {{bold:Contato: }}{{cyan:#{contact[:contact]} }}{{bold:|}} {{bold:CPF: }}{{cyan:#{contact[:cpf]}}}"
   end
+end
+
+def change_contact
+  puts ''
+  puts CLI::UI.fmt '{{bold:Digite o número do CPF do contato que deseja alterar:}}'
+  cpf = cpf_valid
+  # cpf_exist = @address_book.any? { |contact| contact[:cpf] == cpf }
+  e = @address_book.index
+  contact_index = e.each { |contact| contact[:cpf] == cpf }
+  puts CLI::UI.fmt "{{bold:Digite o novo nome para o CPF:}}{{green:#{cpf}}}"
+  new_name = gets.chomp
+  raise CLI::UI.fmt '{{red: O campo novo nome não pode ser vazio. Tente novamente.}}' if new_name.empty?
+
+  puts CLI::UI.fmt "{{bold:Digite o novo contato para o CPF:}}{{green:#{cpf}}}"
+  new_contact = gets.chomp
+  raise CLI::UI.fmt '{{red:O campo novo contato não pode ser vazio. Tente novamente}}' if new_contact.empty?
+
+  # e = @address_book.each_index
+  # a = e.each { |index| puts "#{index} #{@address_book[index]}" }
+  b = { name: new_name, contact: new_contact }
+  c = @address_book[contact_index].merge!(b)
+  puts ''
+  puts CLI::UI.fmt "{{bold:Sucesso}}{{v}}: Você alterou o CPF {{cyan:#{c[:cpf]}}} para o nome {{cyan:#{c[:name]}}} e contato {{cyan:#{c[:contact]}}}"
+  # puts CLI::UI.fmt "{{bold:Nome: }}{{cyan:#{contact[:name]} }}{{bold:|}} {{bold:Contato: }}{{cyan:#{contact[:contact]} }}{{bold:|}} {{bold:CPF: }}{{cyan:#{contact[:cpf]}}}"
+  raise CLI::UI.fmt '{{red: CPF não encontrado. Verifique o dado e tente novamente}}' if contact_index = nil?
 end
 
 def exit_cm
