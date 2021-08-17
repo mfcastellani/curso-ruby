b87462225a846164c4125352e533858e95889732
pauloalchemist <paulolinsdev@gmail.com>
2021-08-17 11:09:19 -0300

    added cpf formatting

--- aula-05/dragonfusion/contact_manager/lib/contact.rb
+++ aula-05/dragonfusion/contact_manager/lib/contact.rb
@@ -9,14 +9,16 @@ def new_contact
   puts CLI::UI.fmt '{{blue:Insira seu telefone ou e-mail:}}'
   contact = gets.chomp
   puts CLI::UI.fmt '{{blue:Insira o seu CPF:}}'
-  cpf = gets.chomp
+  cpf_prompt = gets.chomp
+  cpf = CPF.new(cpf_prompt)
+  cpf_formatted = cpf.formatted
 
-  if @address_book.any? { |contact| contact[:cpf] == cpf }
+  if @address_book.any? { |contact| contact[:cpf] == cpf_formatted }
     raise CLI::UI.fmt '{{red: Este CPF já está cadastrado na sua agenda}}'
   else
-    insert_contact = { name: name, contact: contact, cpf: cpf }
+    insert_contact = { name: name, contact: contact, cpf: cpf_formatted }
     @address_book << insert_contact
-    puts CLI::UI.fmt "{{v}} Inclusão de {{green:#{name}}} com o contato {{green:#{contact}}} e CPF {{green:#{cpf}}} feita com sucesso!"
+    puts CLI::UI.fmt "{{v}} Inclusão de {{green:#{name}}} com o contato {{green:#{contact}}} e CPF {{green:#{cpf_formatted}}} feita com sucesso!"
   end
 end
 
@@ -25,6 +27,24 @@ def listing_all_contacts
   puts ''
   @address_book.each do |contact|
     puts CLI::UI.fmt "{{bold:Nome: }}{{cyan:#{contact[:name]} }}{{bold:|}} {{bold:Contato: }}{{cyan:#{contact[:contact]} }}{{bold:|}} {{bold:CPF: }}{{cyan:#{contact[:cpf]}}}"
+  end
+end
+
+def list_contact_by_key; end
+
+def change_contact_by_cpf
+  puts CLI::UI.fmt '{{bold:Digite o CPF do contato que deseja alterar:}}'
+  cpf = gets.chomp
+  puts CLI::UI.fmt '{{bold:Qual campo deseja alterar: Nome ou email?}}'
+  option = gets.chomp.downcase
+  puts CLI::UI.fmt '{{bold:Alterando contato por CPF:}}'
+  puts ''
+
+  if @address_book.any? { |contact| contact[:cpf] == cpf }
+
+    puts CLI::UI.fmt "{{v}} Nome alterado para {{green:#{name}}} com o contato {{green:#{contact}}} e CPF {{green:#{cpf}}} feita com sucesso!"
+  else
+    raise CLI::UI.fmt '{{red: Este CPF não está cadastrado na sua agenda}}'
   end
 end
 
