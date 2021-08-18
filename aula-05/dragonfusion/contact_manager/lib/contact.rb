44db97b460cf6f849c3e35a5d6fabe435af48695
pauloalchemist <paulolinsdev@gmail.com>
2021-08-17 21:08:46 -0300

    unnecessary lines removed

--- aula-05/dragonfusion/contact_manager/lib/contact.rb
+++ aula-05/dragonfusion/contact_manager/lib/contact.rb
@@ -33,7 +33,7 @@ def change_contact
   puts ''
   puts CLI::UI.fmt '{{bold:Digite o número do CPF do contato que deseja alterar:}}'
   cpf = cpf_valid
-  # cpf_exist = @address_book.any? { |contact| contact[:cpf] == cpf }
+
   e = @address_book.index
   contact_index = e.each { |contact| contact[:cpf] == cpf }
   puts CLI::UI.fmt "{{bold:Digite o novo nome para o CPF:}}{{green:#{cpf}}}"
@@ -44,13 +44,11 @@ def change_contact
   new_contact = gets.chomp
   raise CLI::UI.fmt '{{red:O campo novo contato não pode ser vazio. Tente novamente}}' if new_contact.empty?
 
-  # e = @address_book.each_index
-  # a = e.each { |index| puts "#{index} #{@address_book[index]}" }
   b = { name: new_name, contact: new_contact }
   c = @address_book[contact_index].merge!(b)
   puts ''
   puts CLI::UI.fmt "{{bold:Sucesso}}{{v}}: Você alterou o CPF {{cyan:#{c[:cpf]}}} para o nome {{cyan:#{c[:name]}}} e contato {{cyan:#{c[:contact]}}}"
-  # puts CLI::UI.fmt "{{bold:Nome: }}{{cyan:#{contact[:name]} }}{{bold:|}} {{bold:Contato: }}{{cyan:#{contact[:contact]} }}{{bold:|}} {{bold:CPF: }}{{cyan:#{contact[:cpf]}}}"
+
   raise CLI::UI.fmt '{{red: CPF não encontrado. Verifique o dado e tente novamente}}' if contact_index = nil?
 end
 
