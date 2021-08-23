0be8ad5c2ee64fde48f89665d9642c69bd57273d
Marcelo Castellani <marcelo@linux.com>
2021-08-23 20:51:45 -0300

    Troca os ifs por case

--- aula-extra/compras.rb
+++ aula-extra/compras.rb
@@ -12,30 +12,27 @@ loop do
     print "Digite a opção: "
     opcao = gets.chomp
 
-
-    if opcao == '4'
-        puts "Até mais!"
-        break
-    end 
-
-    if opcao == '1'
+    case opcao
+    when '1'
         print "Insira item a ser adicionado: "
         item = gets.chomp
         lista_compras << item
         puts " Item #{item} adicionado com sucesso"
-    end
-
-    if opcao == '2'
-      print "Digite o item a ser excluído: "
-      item = gets.chomp
-      lista_compras.delete(item)
-      puts "#{item} excluído da lista de compras"
-    end
-    
-    if opcao == '3'
-        lista_compras.each do |item|
-        puts "Item: #{item}"
+    when '2'
+        print "Digite o item a ser excluído: "
+        item = gets.chomp
+        lista_compras.delete(item)
+        puts "#{item} excluído da lista de compras"
+    when '3'
+        lista_compras.sort.each do |item|
+            puts "Item: #{item}"
         end
+    when '4'
+        puts "Até mais!"
+        break
+    else
+        puts 'Você digitou uma opção inválida!!!!!'
     end
 
+    puts ''
 end
