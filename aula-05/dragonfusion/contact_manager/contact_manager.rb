7b784568f6481e15b218fd619483fde037a3703d
pauloalchemist <paulolinsdev@gmail.com>
2021-08-24 14:56:01 -0300

    modified access with class Contact

--- aula-05/dragonfusion/contact_manager/contact_manager.rb
+++ aula-05/dragonfusion/contact_manager/contact_manager.rb
@@ -8,16 +8,18 @@ end
 
 print box
 
+manager = Contact.new
+
 loop do
   puts ''
   CLI::UI::Prompt.ask('Escolha qual ação quer realizar na agenda 👇') do |handler|
-    handler.option('Incluir uma pessoa') { new_contact }
+    handler.option('Incluir uma pessoa') { manager.new_contact }
     handler.option('Consultar todos os registros') { listing_all_contacts }
     handler.option('Alterar apenas o nome de uma pessoa') { change_contact_name }
     handler.option('Alterar apenas o meio de contato de uma pessoa') { change_form_contact }
     handler.option('Alterar nome e contato de uma pessoa') { change_contact }
     handler.option('Excluir os dados de uma pessoa') { delete_options }
-    handler.option('Sair') { exit_cm }
+    handler.option('Sair') { manager.exit_cm }
   end
 rescue StandardError => e
   puts CLI::UI.fmt "⚠️ {{underline:Ocorreu um erro:}}#{e.message}"
