fc06fc5153a7a4a6942c197184d1b39d142d65f6
pauloalchemist <paulolinsdev@gmail.com>
2021-08-24 20:38:54 -0300

    deleted create_table_contacts

--- aula-05/dragonfusion/contact_manager/database/queries/create_table_contacts.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../connection_db'
-
-db.execute <<-SQL
-  create table if not exists contacts (
-    name varchar(100) not null,
-    means_contact varchar(50) not null,
-    cpf varchar(5) not null unique
-  );
-SQL
