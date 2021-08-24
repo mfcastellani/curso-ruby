97c6802cc2ad10705b077ad1a90eddfad6eae03d
pauloalchemist <paulolinsdev@gmail.com>
2021-08-24 15:16:54 -0300

    first test passed with query in database

--- aula-05/dragonfusion/contact_manager/tests/search_one_contact_spec.rb
+++ aula-05/dragonfusion/contact_manager/tests/search_one_contact_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Contact do
   context 'testing search one contact' do
     let(:manager) { Contact.new }
     it 'return only one contact' do
+      cpf = '18295324306'
+      test1 = db.execute("select name, means_contact, cpf from contacts where cpf = #{cpf}")
+
+      expect(test1[0][2]).to eq('18295324306')
     end
   end
 end
