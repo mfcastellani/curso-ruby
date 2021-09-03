5b6dda6d015d0ab97bd674ebe69bd12a8a628d52
lucas lucatero <lcspontes@gmail.com>
2021-09-03 12:25:35 -0300

    Implementaçao do ponto 6.3 - Criando um novo Artigo

--- aula-08/grupo2/blog-grupo2/app/controllers/articles_controller.rb
+++ aula-08/grupo2/blog-grupo2/app/controllers/articles_controller.rb
@@ -6,4 +6,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article      
+    else
+      render :new      
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
