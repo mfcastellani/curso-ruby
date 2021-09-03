20e98a9677f6012a2aad0bd8c9f8bcf3a3a1b09b
lucas lucatero <lcspontes@gmail.com>
2021-09-03 11:31:36 -0300

    Create route parameters, add the action show to app/controllers, create file show.html.erb, add link to articles on index

--- aula-08/grupo2/blog-grupo2/config/routes.rb
+++ aula-08/grupo2/blog-grupo2/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
+
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
