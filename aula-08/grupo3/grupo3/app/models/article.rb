49014713246f0bf2f5f379ee755af2268a207587
Caio Victor <caiovdamorim@gmail.com>
2021-09-04 21:13:07 -0300

    Continuar 8

--- aula-08/grupo3/grupo3/app/models/article.rb
+++ aula-08/grupo3/grupo3/app/models/article.rb
@@ -2,3 +2,10 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
+
+class Article < ApplicationRecord
+  has_many :comments
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
