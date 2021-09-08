ee290b553af2c4d020860948e03b24bcf5d5dbdf
lucas lucatero <lcspontes@gmail.com>
2021-09-08 11:46:03 -0300

    6.5 Added delete articles feature

--- aula-08/grupo2/blog-grupo2/app/controllers/articles_controller.rb
+++ aula-08/grupo2/blog-grupo2/app/controllers/articles_controller.rb
@@ -35,6 +35,13 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
